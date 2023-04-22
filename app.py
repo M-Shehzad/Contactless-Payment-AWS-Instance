9eaaa6b172726c76a7e48ea595b789b9ec49b98e
pravin-pk <19cs119.pravin@sjec.ac.in>
2023-04-22 12:22:46 +0530

    ip hosting and port changes

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def predict():
     return json.dumps({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8888)
