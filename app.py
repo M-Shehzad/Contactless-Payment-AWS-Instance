3d7b9a98efc29952318fc452bf1ecf5752520cb8
Ubuntu <ubuntu@ip-172-31-11-235.ap-south-1.compute.internal>
2023-04-23 15:56:23 +0000

    updating vector as string list

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import boto3
 import numpy as np
 import json
 import tensorflow as tf
+import time
 
 app = Flask(__name__)
 
@@ -10,9 +11,10 @@ model = tf.keras.models.load_model('./model/0000001')
 
 table = boto3.resource('dynamodb',region_name='ap-south-1').Table('palmPrints')
 
-def putDynamoDB(imageName, vector):
+def putDynamoDB(data):
     try:
-        item = {'imageName': imageName, 'vector': vector}
+        timestamp = int(time.time())
+        item = {'timestamp': timestamp} | data
         table.put_item(Item=item)
         return True
     except Exception as e:
@@ -32,12 +34,11 @@ def predict():
     image = np.array(data['image'])
     imageName = data['imageName']
 
-    prediction = model.predict(image)
-    predictionString = ",".join([str(i) for i in prediction])
-    
-    DBstatus = putDynamoDB(imageName,predictionString)
+    prediction = model.predict(image).tolist()
 
-    return json.dumps({'prediction': prediction.tolist(), 'DBstatus': DBstatus})
+    DBstatus = putDynamoDB({'imageName': imageName, 'vector': [str(i) for i in prediction]})
+
+    return json.dumps({'prediction': prediction, 'DBstatus': DBstatus})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8888)
